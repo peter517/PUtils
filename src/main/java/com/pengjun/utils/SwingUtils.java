43df94a70d316a5438e98681e785021567ec2c59
PenGuin@home <peter517@126.com>
2013-10-21 22:05:47 +0800

    update

--- src/main/java/com/pengjun/utils/SwingUtils.java
+++ src/main/java/com/pengjun/utils/SwingUtils.java
@@ -8,22 +8,17 @@ public class SwingUtils {
 
 	public static void setFrameCenter(JFrame frame) {
 
-		int screenWidth = (int) Toolkit.getDefaultToolkit().getScreenSize()
-				.getWidth();
-		int screenHeight = (int) Toolkit.getDefaultToolkit().getScreenSize()
-				.getHeight();
-		frame.setLocation((screenWidth - frame.getWidth()) / 2,
-				(screenHeight - frame.getHeight()) / 2);
+		int screenWidth = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		int screenHeight = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		frame.setLocation((screenWidth - frame.getWidth()) / 2, (screenHeight - frame.getHeight()) / 2);
 
 	}
 
-	public static JFrame createFrame(int width, int height) {
+	public static void createFrame(JFrame frame, int width, int height) {
 
-		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(width, height);
 		frame.setVisible(true);
 
-		return frame;
 	}
 }
