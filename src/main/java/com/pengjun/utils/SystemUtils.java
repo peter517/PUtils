44e6ea5664ea2a9f30910caf1ac2abdd33a3cf7b
mmp <mmp@mmpc.local>
2013-11-20 19:49:56 +0800

    add remotectrl

--- src/main/java/com/pengjun/utils/SystemUtils.java
+++ src/main/java/com/pengjun/utils/SystemUtils.java
@@ -3,48 +3,7 @@ package com.pengjun.utils;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.content.Context;
-import android.media.AudioManager;
-
 public class SystemUtils {
-
-	public static void setSpeakerVolume(Context context, int level) {
-		if (context == null) {
-			return;
-		}
-		AudioManager audioManager = (AudioManager) context
-				.getSystemService(Context.AUDIO_SERVICE);
-
-		if (audioManager != null) {
-			audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, level, 0);
-		}
-	}
-
-	public static int getCurrnetSpeakerVolume(Context context) {
-
-		if (context == null) {
-			return -1;
-		}
-		AudioManager audioManager = (AudioManager) context
-				.getSystemService(Context.AUDIO_SERVICE);
-		return audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-	}
-
-	public static int getMaxSpeakerVolume(Context context) {
-
-		if (context == null) {
-			return -1;
-		}
-
-		int level = -1;
-		AudioManager audioManager = (AudioManager) context
-				.getSystemService(Context.AUDIO_SERVICE);
-
-		if (audioManager != null) {
-			level = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-		}
-		return level;
-	}
 
 	public static List<StringBuffer> getCurThreadListInfo() {
 		ThreadGroup group = Thread.currentThread().getThreadGroup();
