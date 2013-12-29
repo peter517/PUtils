6136cdcf20f8e0a6911cc7a960a7b18732f5375c
PenGuin@home <peter517@126.com>
2013-12-29 22:32:09 +0800

    add base64

--- src/main/java/com/pengjun/utils/StringUtils.java
+++ src/main/java/com/pengjun/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package com.pengjun.utils;
 
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.MessageDigest;
@@ -12,6 +13,30 @@ import java.util.regex.Pattern;
 public class StringUtils {
 
 	public static final String NULL_STRING = "";
+	
+	/***
+	 * encode by Base64
+	 */
+	public static String encodeBase64(byte[] input) throws Exception {
+		Class clazz = Class
+				.forName("com.sun.org.apache.xerces.internal.impl.dv.util.Base64");
+		Method mainMethod = clazz.getMethod("encode", byte[].class);
+		mainMethod.setAccessible(true);
+		Object retObj = mainMethod.invoke(null, new Object[] { input });
+		return (String) retObj;
+	}
+
+	/***
+	 * decode by Base64
+	 */
+	public static byte[] decodeBase64(String input) throws Exception {
+		Class clazz = Class
+				.forName("com.sun.org.apache.xerces.internal.impl.dv.util.Base64");
+		Method mainMethod = clazz.getMethod("decode", String.class);
+		mainMethod.setAccessible(true);
+		Object retObj = mainMethod.invoke(null, input);
+		return (byte[]) retObj;
+	}
 
 	public static <T extends Enum<T>> String[] getEnumStrArr(Class<T> enumClass) {
 		String[] enumStrArr = new String[enumClass.getEnumConstants().length];
